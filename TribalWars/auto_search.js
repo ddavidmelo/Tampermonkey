0cdc77a8491678286a0931cbdfda7863c006287a
ddavidmelo <ddavidmeloo@gmail.com>
2020-05-15 21:12:38 +0100

    Update auto_search.js

--- TribalWars/auto_search.js
+++ TribalWars/auto_search.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Script Power
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  try to take over the world!
 // @author       David
 // @match        https://pt73.tribalwars.com.pt/*
