3ff5902fc653f757f36518d9ffd89c8aa7ff2060
ddavidmelo <ddavidmeloo@gmail.com>
2020-07-10 21:22:45 +0100

    Update auto_searchPT74.js

--- TribalWars/auto_searchPT74.js
+++ TribalWars/auto_searchPT74.js
@@ -4,7 +4,7 @@
 // @version      1.10
 // @description  try to take over the world!
 // @author       David
-// @match        https://pt73.tribalwars.com.pt/*
+// @match        https://pt74.tribalwars.com.pt/*
 // @include      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @icon         https://dspt.innogamescdn.com/asset/b56a168/graphic/favicon.png
 // @updateURL    https://raw.githubusercontent.com/ddavidmelo/Tampermonkey/master/TribalWars/auto_search.js
@@ -17,19 +17,21 @@
     var c = 0;
     var server_info = document.getElementById("serverDate");
     server_info.innerHTML = server_info.innerHTML + " <span> | </span> <span id=\"counter\"></span>";
-    var troops_go =[[0   ,   0,   0,   0,   0,  0],     // Weak Search
-                    [0   ,   0,   0,   0,   0,  0],     // Humble Search
-                    [0   ,   0,   0,   0,   0,  0],     // Smart Search
-                    [0   ,   0,   0,   0,   0,  0]];    // Extreme Search
-                  // lan   esp   vik  lev  pes  pala 
+    var troops_go =[[0   ,   0,   0,   0,   100,   0,   0,  0],     // Weak Search
+                    [0   ,   0,   0,   0,   0,   0,   0,  0],     // Humble Search
+                    [111 ,  30,   0,   0,   0,   0,   0,  0],     // Smart Search
+                    [0   ,   0,   0,   0,   0,   0,   0,  0]];    // Extreme Search
+                  // lan   esp   vik arch  lev  levAR pes pala
 
     function select_troops(x) {
         $("[name='spear']").val(troops_go[x][0]).trigger('change');
         $("[name='sword']").val(troops_go[x][1]).trigger('change');
         $("[name='axe']").val(troops_go[x][2]).trigger('change');
-        $("[name='light']").val(troops_go[x][3]).trigger('change');
-        $("[name='heavy']").val(troops_go[x][4]).trigger('change');
-        $("[name='knight']").val(troops_go[x][5]).trigger('change');
+        $("[name='arch']").val(troops_go[x][3]).trigger('change');
+        $("[name='light']").val(troops_go[x][4]).trigger('change');
+        $("[name='marcher']").val(troops_go[x][5]).trigger('change');
+        $("[name='heavy']").val(troops_go[x][6]).trigger('change');
+        $("[name='knight']").val(troops_go[x][7]).trigger('change');
         console.log("Troops Selected");
         $(".status-specific").slice(x,x+1).find(".btn.btn-default.free_send_button").trigger('click');
     }
